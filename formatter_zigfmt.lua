d9b37af44e400eb4ec817991a3e047fc063ebfe5
Tommi Sinivuo <600815+TommiSinivuo@users.noreply.github.com>
2021-05-17 11:15:35 +0300

    Add formatter for zig fmt
    Zig fmt is the built in formatter of the Zig language compiler.

---
+++ formatter_zigfmt.lua
@@ -0,0 +1,13 @@
+-- lite-xl 1.16
+-- for zig fmt formatter
+local config = require "core.config"
+local formatter = require "plugins.formatter"
+ 
+config.zigfmt_args = {} -- zig fmt doesn't need any args when formatting single file
+
+formatter.add_formatter {
+    name = "zig fmt",
+    file_patterns = {"%.zig$"},
+    command = "zig fmt $ARGS $FILENAME",
+    args = config.zigfmt_args
+}
