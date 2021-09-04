077e7e7d1bc690850a7ed7b367bf2a7b83d83edc
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2021-09-05 02:42:50 +0530

    Add a Julia Language formatter

---
+++ formatter_juliaformatter.lua
@@ -0,0 +1,13 @@
+-- mod-version:2 lite-xl 2.00
+-- for JuliaFormatter formatter
+local config = require "core.config"
+local formatter = require "plugins.formatter"
+ 
+config.julia_args = {}
+
+formatter.add_formatter {
+    name = "JuliaFormatter",
+    file_patterns = {"%.jl$", "%.julia$"},
+    command = "julia --compile=min -e $ARGS 'import JuliaFormatter: format_file; format_file($FILENAME)'",
+    args = config.julia_args
+}
