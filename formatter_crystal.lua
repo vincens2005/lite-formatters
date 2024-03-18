5f2f2e7c8dc18a005269e3ae12043df0be746603
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-03-18 23:24:03 +0100

    Fixed formatter comment.

--- formatter_crystal.lua
+++ formatter_crystal.lua
@@ -1,5 +1,5 @@
 -- mod-version:3 lite-xl 2.1
--- for the official Crystal formatter
+-- for Crystal's official formatter
 local config = require "core.config"
 local formatter = require "plugins.formatter"
 
