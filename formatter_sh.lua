423b7ca83def8ce5106c5293c00cec6e955eb418
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-03-18 13:44:37 +0100

    Added bash formatter via shfmt.

---
+++ formatter_sh.lua
@@ -0,0 +1,13 @@
+-- mod-version:3 lite-xl 2.1
+-- https://github.com/mvdan/sh?tab=readme-ov-file
+local config = require "core.config"
+local formatter = require "plugins.formatter"
+
+config.sh_args = {"-l", "-w"}
+
+formatter.add_formatter {
+	name = "shfmt",
+	file_patterns = {"%.sh$"},
+	command = "shfmt $ARGS $FILENAME",
+	args = config.sh_args
+}
