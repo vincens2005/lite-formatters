b1bee26e533400c5b491dddb217a421976af6a46
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-09-26 15:23:44 +0200

    Fixed command arguments.

--- formatter_prettier.lua
+++ formatter_prettier.lua
@@ -4,11 +4,13 @@
 local formatter = require "plugins.formatter"
 local config = require "core.config"
 
+config.prettier_args = {"--write"}
+
 formatter.add_formatter {
     name = "Prettier",
-    file_patterns = {"%.less&", "%.scss&", "%.json&", "%.jsx?$", "%.ts$"},
-    command = "prettier $ARGS $FILENAME",
-    args = { "-w" }
+    file_patterns = {"%.less$", "%.scss$", "%.json$", "%.jsx?$", "%.ts$"},
+    command = "npx prettier $ARGS $FILENAME",
+    args = config.prettier_args
 }
 
 config.format_on_save = true
